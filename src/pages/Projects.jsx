706f29fa33a7dcfca7f29469680025999ca3a748
ibrahim jomaa <78326620+divluffy@users.noreply.github.com>
2021-12-17 16:21:17 +0200

    Add files via upload

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,17 +1,9 @@
 import React from "react";
-import "../components/project.css";
 import MyCodes from "../components/MyCodes";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 function Projects() {
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 800,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
   return (
     <div>
       <div className="bowl_title_projects">
@@ -27,42 +19,32 @@ function Projects() {
       </div>
 
       <div className="bowl_slider_project">
-        <Slider {...settings}>
+        <div className="content_Bowl_slider">
           {MyCodes.map((pro, i) => {
             return (
-              <div className="card_4_cards">
-                {pro.map((four, i) => {
-                  return (
-                    <div className="bowl_projects">
-                      <div className="banar_projects">
-                        <img src={four.banar} alt="" />
-                      </div>
-                      <div className="bowl_info_t_project">
-                        <div className="line_prop">
-                          <a target="_blank" href={four.demo} rel="noreferrer">
-                            Demo
-                          </a>
-                        </div>
-                        <div className="line_prop">
-                          <a
-                            target="_blank"
-                            href={four.source}
-                            rel="noreferrer"
-                          >
-                            Source code
-                          </a>
-                        </div>
-                        <div className="line_prop">
-                          <span>{four.tools}</span>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
+              <div className="bowl_projects">
+                <div className="banar_projects">
+                  <img src={pro.banar} alt="" />
+                </div>
+                <div className="bowl_info_t_project">
+                  <div className="line_prop">
+                    <span>{pro.name}</span>
+                  </div>
+                  <div className="line_prop">
+                    <a target="_blank" href={pro.demo} rel="noreferrer">
+                      Demo
+                    </a>
+                  </div>
+                  <div className="line_prop">
+                    <a target="_blank" href={pro.source} rel="noreferrer">
+                      Source code
+                    </a>
+                  </div>
+                </div>
               </div>
             );
           })}
-        </Slider>
+        </div>
       </div>
     </div>
   );
