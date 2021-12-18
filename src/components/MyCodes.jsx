58fefc232b980d01bc244dbf678158fef66b6feb
ibrahim jomaa <78326620+divluffy@users.noreply.github.com>
2021-12-18 23:01:08 +0200

    Add files via upload

---
+++ src/components/MyCodes.jsx
@@ -0,0 +1,76 @@
+const a = [
+  {
+    id: 0,
+    name: "PSJobs",
+    demo: "https://psjobs.herokuapp.com/",
+    source: "https://github.com/divluffy/PSJobs",
+    tools: "React, Node, PostgreSQL",
+    banar:
+      "https://github.com/divluffy/image-upload/blob/main/psjobs.PNG?raw=true",
+  },
+  {
+    id: 1,
+    name: "clone-facebook",
+    demo: "https://divluffy.github.io/clone-facebook/home/home.html",
+    source: "https://github.com/divluffy/clone-facebook",
+    tools: "Html, Css, JS",
+    banar:
+      "https://github.com/divluffy/image-upload/blob/main/fb%20clone.PNG?raw=true",
+  },
+  {
+    id: 2,
+    name: "GSG home",
+    demo: "https://divluffy.github.io/GSGhome/",
+    source: "https://github.com/divluffy/GSGhome",
+    tools: "Html, Css, JS",
+    banar:
+      "https://github.com/divluffy/image-upload/blob/main/gsg.PNG?raw=true",
+  },
+  {
+    id: 3,
+    name: "anime search",
+    demo: "https://divluffy.github.io/animeSearch/",
+    source: "https://github.com/divluffy/animeSearch",
+    tools: "Html, Css, JS",
+    banar:
+      "https://github.com/divluffy/image-upload/blob/main/an%20ime.PNG?raw=true",
+  },
+  {
+    id: 0,
+    name: "PSJobs",
+    demo: "https://psjobs.herokuapp.com/",
+    source: "https://github.com/divluffy/PSJobs",
+    tools: "React, Node, PostgreSQL",
+    banar:
+      "https://github.com/divluffy/image-upload/blob/main/psjobs.PNG?raw=true",
+  },
+  {
+    id: 1,
+    name: "clone-facebook",
+    demo: "https://divluffy.github.io/clone-facebook/home/home.html",
+    source: "https://github.com/divluffy/clone-facebook",
+    tools: "Html, Css, JS",
+    banar:
+      "https://github.com/divluffy/image-upload/blob/main/fb%20clone.PNG?raw=true",
+  },
+  {
+    id: 2,
+    name: "GSG home",
+    demo: "https://divluffy.github.io/GSGhome/",
+    source: "https://github.com/divluffy/GSGhome",
+    tools: "Html, Css, JS",
+    banar:
+      "https://github.com/divluffy/image-upload/blob/main/gsg.PNG?raw=true",
+  },
+  {
+    id: 3,
+    name: "anime search",
+    demo: "https://divluffy.github.io/animeSearch/",
+    source: "https://github.com/divluffy/animeSearch",
+    tools: "Html, Css, JS",
+    banar:
+      "https://github.com/divluffy/image-upload/blob/main/an%20ime.PNG?raw=true",
+  },
+];
+
+export default a;
